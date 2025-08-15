1f79f61c3e1e73a569a2574b7acd3ed13f18eeef
2044-space-elevator <spaceelevator2044@126.com>
2025-08-15 14:56:59 +0800

    [feat]添加设置功能，响铃提示功能

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
+import keyboard
 import socket
 import tkinter as tk
+import winsound
 import threading as thread
 from sys import exit
 
-root = tk.Tk()
-root.title("Connect to IP:")
 
 CONNECT_IP = None
 USER_NAME = None
@@ -12,8 +12,69 @@ USER_input = None
 IP_input = None
 PORT_input = None
 exit_flg = 0
+BELL = False
+text_box = None
+FONT_FAMILY = ("微软雅黑", 20, "")
+exit_flg_2 = False
+
+def setting():
+    global exit_flg_2
+    exit_flg_2 = False
+    set_tk = tk.Toplevel()
+    set_tk.title("设置")
+    font_label = tk.Label(set_tk, text='字体名称')
+    font_label.pack()
+    font_entry = tk.Entry(set_tk) 
+    font_entry.insert(0, FONT_FAMILY[0])
+    font_entry.pack()
+    
+    font_size_label = tk.Label(set_tk, text="字号")
+    font_size_label.pack()
+    font_size_entry = tk.Entry(set_tk)
+    font_size_entry.insert(0, FONT_FAMILY[1])
+    font_size_entry.pack()
+
+    ini_val = tk.BooleanVar(value=BELL) 
+    bell_on_button = tk.Checkbutton(set_tk, text="是否开启语音提示（仅 Windows 支持）", variable=ini_val, onvalue=True, offvalue=False)
+    bell_on_button.pack()
+
+    def confirm_f():
+        global exit_flg_2 
+        global BELL
+        global FONT_FAMILY
+        try:
+            fsize = int(font_size_entry.get())
+            fname = font_entry.get()
+            FONT_FAMILY = (fname, fsize, "")
+            text_box.configure(font=FONT_FAMILY)
+            BELL = ini_val.get()
+        except:
+            pass
+
+        exit_flg_2 = True
+
+    confirm = tk.Button(set_tk, text="确定", command=confirm_f)
+    confirm.pack()
+
+    def close_window():
+        global exit_flg_2
+        exit_flg_2 = True
+    set_tk.protocol('WM_DELETE_WINDOW', close_window)
+
+    while True:
+        set_tk.update()
+        set_tk.update_idletasks()
+        if exit_flg_2:
+            set_tk.destroy()
+            return
+
+
+root = tk.Tk()
+root.title("Connect to IP:")
+    
 
 def show():
+    global text_box
     s = socket.socket()
     USER_NAME = USER_input.get()
     CONNECT_IP = IP_input.get()
@@ -36,6 +97,8 @@ def show():
             rcv_data = s.recv(1024).decode("utf-8")
         except:
             return
+        if (USER_NAME + ":" not in rcv_data) and rcv_data and BELL:
+            winsound.Beep(1000,440)
         text_box.insert('end', rcv_data)
         text_box.see("end")
 
@@ -61,6 +124,8 @@ def show():
     but = tk.Button(rt, text="发送", command=send_msg)
     msg_input.pack()
     but.pack()
+    but_set = tk.Button(rt, text="设置", command=setting)
+    but_set.pack()
 
     def close_window():
         global exit_flg
@@ -75,6 +140,8 @@ def show():
         rt.update_idletasks()
         rt.update()
         receive_msg()
+        if keyboard.is_pressed('ctrl') and keyboard.is_pressed('1'):
+            send_msg()
 
 label = tk.Label(root, text="Connect to IP:")
 IP_input = tk.Entry(root, width=20)
@@ -84,6 +151,10 @@ lb3 = tk.Label(root, text="Connect to port")
 PORT_input = tk.Entry(root, width=20)
 
 but = tk.Button(root, text="确定", command=show)
+
+hint = tk.Label(root, text="提示：同时按下 Ctrl+1 可以发送信息")
+set_hint2 = tk.Label(root, text="打开设置时，主窗口将暂停进程")
+
 label.pack()
 IP_input.pack()
 lb2.pack()
@@ -91,4 +162,6 @@ USER_input.pack()
 lb3.pack()
 PORT_input.pack()
 but.pack()
+hint.pack()
+set_hint2.pack()
 root.mainloop()
